a992246d87c6c58b33faf6c7c13d42c8e44e46fb
nimomaina <nimomaina.nm@gmail.com>
2019-03-08 15:45:45 +0300

    Create views and landing page

--- insta/urls.py
+++ insta/urls.py
@@ -7,8 +7,4 @@ from . import views
 urlpatterns=[
 
     url('^$',views.home,name='home'),
-
 ]
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+import datetime as dt
+from . models import *
 # Create your views here.
+
+
+
+def home(request):
+    return render(request,'home.html')
