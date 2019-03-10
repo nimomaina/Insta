8f95126f5f187e26ba81cbb28cef2f8076265477
nimomaina <nimomaina.nm@gmail.com>
2019-03-10 19:01:15 +0300

    Edit settings

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
 from . models import *
@@ -8,9 +9,6 @@ from . models import *
 @login_required(login_url='/accounts/login/')
 def home(request):
     current_user = request.user
-    all_images = Image.objects.all()
-    comments = Comment.objects.all()
-    likes = Likes.objects.all
+    all_images = Picture.objects.all()
     profile = Profile.objects.all()
-    print(likes)
     return render(request,'home.html',locals())
