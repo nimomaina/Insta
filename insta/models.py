931918ab962254482136ca70489b3a0b3cbbab51
nimomaina <nimomaina.nm@gmail.com>
2019-03-10 18:45:43 +0300

    Create profile

--- insta/models.py
+++ insta/models.py
@@ -3,12 +3,42 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Profile(models.Model):
+    profile_image =models.ImageField( blank=True)
+    bio = models.CharField(max_length=255)
+    owner = models.OneToOneField(User,blank=True, on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return str(self.bio)
+
+
+    def profile_save(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(owner=id)
+        return profile
+
+    @classmethod
+    def get_profile_by_username(cls, owner):
+        profiles = cls.objects.filter(owner__contains=owner)
+        return profiles
+
+
 class Picture(models.Model):
-    name = models.CharField(max_length=30)
     image = models.ImageField(upload_to='uploads/', default='default.jpg')
-    description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=30)
+    caption = models.TextField(blank=True)
+    profile = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    profile_details = models.ForeignKey(Profile)
+
+    def __str__(self):
+        return str(self.name)
+
 
     def save_pic(self):
         self.save()
@@ -17,15 +47,6 @@ class Picture(models.Model):
         self.delete()
 
     @classmethod
-    def search_by_category(cls,category):
-        pic = cls.objects.filter(category__name__icontains = category)
-        return pic
-
-    @classmethod
-    def filter_by_location(cls, location):
-        pic = cls.objects.filter(location__name__icontains=location)
-        return pic
-
     def get_image_by_id(id):
         pic = Picture.objects.get(id)
         return pic
