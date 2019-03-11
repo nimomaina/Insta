daea73b6906827b1437c74766f580afa1a01932e
nimomaina <nimomaina.nm@gmail.com>
2019-03-12 01:31:03 +0300

    Correct errors in models

--- insta/models.py
+++ insta/models.py
@@ -65,36 +65,3 @@ class Picture(models.Model):
     def get_image_by_id(id):
         pic = Picture.objects.get(id)
         return pic
-
-class Comment(models.Model):
-    pic = models.ForeignKey(Picture,blank=True, on_delete=models.CASCADE,related_name='comment')
-    comment_owner = models.ForeignKey(User, blank=True)
-    comment= models.CharField(max_length=255)
-
-    def save_comment(self):
-        self.save()
-
-    def delete_comment(self):
-        self.delete()
-
-    @classmethod
-    def get_image_comments(cls, id):
-        comments = Comment.objects.filter(pic__pk=id)
-        return comments
-
-    def __str__(self):
-        return str(self.comment)
-
-class Like(models.Model):
-    pic = models.ForeignKey(Picture)
-    user = models.ForeignKey(User)
-
-    class Meta:
-        unique_together = ("post", "user")
-
-    def __str__(self):
-        return 'Like: ' + self.user.username + ' ' + self.post.title
-
-class Followers(models.Model):
-    user = models.CharField(max_length=20,default='')
-    Follower = models.CharField(max_length=20,default='')
